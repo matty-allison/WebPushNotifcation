005bbd98b31dbad2cddb74c7fb6e80b05e775983
matty-allison <mattymallison@gmail.com>
2022-02-22 09:46:15 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebaseMessaging from "./firebase/firebase";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.config.globalProperties.$messaging = firebaseMessaging;
+app.use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Messaging /> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Messaging from "@/components/Messaging.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Messaging,
   },
 };
 </script>
